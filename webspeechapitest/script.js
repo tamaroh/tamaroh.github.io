f7530c5b711d5b6b9adad2c254cad56f5327d1aa
tamaroh <tamaroh@gmail.com>
2021-12-01 15:29:28 +0900

    add webspeechtest

---
+++ webspeechapitest/script.js
@@ -0,0 +1,21 @@
+const speech = new webkitSpeechRecognition();
+    speech.lang = 'ja-JP';
+
+    const btn = document.getElementById('btn');
+    const content = document.getElementById('content');
+
+    btn.addEventListener('click' , function() {
+        speech.start();
+    });
+
+    speech.onresult = function(e) {
+         speech.stop();
+         if(e.results[0].isFinal){
+             var autotext =  e.results[0][0].transcript
+             content.innerHTML += '<div>'+ autotext +'</div>';
+          }
+     }
+
+     speech.onend = () => { 
+        speech.start() 
+     };
