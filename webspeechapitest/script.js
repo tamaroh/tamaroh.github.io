fa4cb21751f532ea636aa343f6f2eba53300ae4a
tamaroh <tamaroh@gmail.com>
2021-12-06 22:30:59 +0900

    added set-apitoken

--- webspeechapitest/script.js
+++ webspeechapitest/script.js
@@ -1,21 +1,38 @@
 const speech = new webkitSpeechRecognition();
-    speech.lang = 'ja-JP';
+speech.lang = 'ja-JP';
 
-    const btn = document.getElementById('btn');
-    const content = document.getElementById('content');
+const btn = document.getElementById('btn');
+const content = document.getElementById('content');
+let zoomApiToken;
+let seq;
+btn.addEventListener('click' , function() {
+    // save token
+    zoomApiToken = document.getElementById("apitoken").value;
+    // reset seq
+    seq = 0;
+    speech.start();
+});
 
-    btn.addEventListener('click' , function() {
-        speech.start();
-    });
+speech.onresult = function(e) {
+        speech.stop();
+        if(e.results[0].isFinal){
+            let autotext =  e.results[0][0].transcript
+            // post to zoom
+            const url = zoomApiToken + '&seq=' + seq + '&lang=' + speech.lang;
+            fetch (url, autotext)
+            .then (res => {
+                content.innerHTML += '<div>'+ autotext +'</div>';
+                seq ++;
+            } )
+            .catch((res, err) => {
+                console.log(err);
+            })
+            // if responce is not error
+            //     seq ++
+            // else retry
+        }
+    }
 
-    speech.onresult = function(e) {
-         speech.stop();
-         if(e.results[0].isFinal){
-             var autotext =  e.results[0][0].transcript
-             content.innerHTML += '<div>'+ autotext +'</div>';
-          }
-     }
-
-     speech.onend = () => { 
-        speech.start() 
-     };
+    speech.onend = () => { 
+    speech.start() 
+    };
