ead219bb23da2fa5db59acf0bf28a75f099cc7f6
tamaroh <tamaroh@gmail.com>
2021-12-15 12:28:04 +0900

    loop fix

--- webspeechapitest/zoomapi.js
+++ webspeechapitest/zoomapi.js
@@ -7,19 +7,18 @@ btn.addEventListener('click' , function() {
     zoomApiToken = document.getElementById("apitoken").value;
     // reset seq
     seq = 0;
+    for (let i = 0; i < 10; i++) {
+        const message = `メッセージテスト: i = ${i}, seq = ${seq}`;
+        const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
+        fetch (url, {
+            method: 'POST',
+            body:message
+        })
+        .then (res => {
+            console.log(res);
+            content.innerHTML += '<div>'+ message +'</div>\r\n';
+            seq ++;
+            return res;
+        });
+    }
 });
-
-for (let i = 0; i < 10; i++) {
-    const message = `メッセージテスト: i = ${i}, seq = ${seq}`;
-    const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
-    fetch (url, {
-        method: 'POST',
-        body:message
-    })
-    .then (res => {
-        console.log(res);
-        content.innerHTML += '<div>'+ message +'</div>\r\n';
-        seq ++;
-        return res;
-    });
-}
