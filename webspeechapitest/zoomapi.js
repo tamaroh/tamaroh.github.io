11e1a4f942b1b66f0124b8f2fdb0d046e6c55b1c
tamaroh <tamaroh@gmail.com>
2021-12-15 13:28:28 +0900

    added no-cors

--- webspeechapitest/zoomapi.js
+++ webspeechapitest/zoomapi.js
@@ -12,13 +12,28 @@ btn.addEventListener('click' , function() {
         const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
         fetch (url, {
             method: 'POST',
-            body:message
+            mode: 'no-cors',
+            body: message
         })
         .then (res => {
             console.log(res);
+            console.log(res.ok);
             content.innerHTML += '<div>'+ message +'</div>\r\n';
             seq ++;
             return res;
         });
     }
 });
+
+/**
+ * Access to fetch at 'https://us02wmcc.zoom.us/closedcaption?id=89390871051&ns=VGFtYXJvaOOBrlpvb23jg5_jg7zjg4bjgqPjg7Pj&expire=86400&sparams=id%2Cns%2Cexpire&signature=-_eY5udIbx4SzvD9pRnoKZhoruaGKDe63KNO5dq6tEM.AG.aDzLw9vAHfsIfYBKnHSAPuUyHJXXvBybiFJxcPAb5PST-8JUDsvdDtQBNmy-HPnFqe84HWjGYPRCMLNLp1ksFfZ0dg2dCjPYmSq4-uIP3RFFmvQZVBtzmwQF629EC80B5ExAPxBYRHY7tvZV3E2KmZFhMtnn5M4r.9sRJBvBjc4iiuN8r7nqtig.JYwFIDJgmKR7zmUH&seq=0&lang=ja-JP' 
+ * from origin 'https://tamaroh.github.io' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+zoomapi.js:13 
+
+POST https://us02wmcc.zoom.us/closedcaption?id=89390871051&ns=VGFtYXJvaOOBrlpvb23jg5_jg7zjg4bjgqPjg7Pj&expire=86400&sparams=id%2Cns%2Cexpire&signature=-_eY5udIbx4SzvD9pRnoKZhoruaGKDe63KNO5dq6tEM.AG.aDzLw9vAHfsIfYBKnHSAPuUyHJXXvBybiFJxcPAb5PST-8JUDsvdDtQBNmy-HPnFqe84HWjGYPRCMLNLp1ksFfZ0dg2dCjPYmSq4-uIP3RFFmvQZVBtzmwQF629EC80B5ExAPxBYRHY7tvZV3E2KmZFhMtnn5M4r.9sRJBvBjc4iiuN8r7nqtig.JYwFIDJgmKR7zmUH&seq=0&lang=ja-JP
+ net::ERR_FAILED 403
+(anonymous) @ zoomapi.js:13
+
+zoomapi.js:13   Uncaught (in promise) TypeError: Failed to fetch
+    at HTMLButtonElement.<anonymous> (zoomapi.js:13)
+ */
