a532a7408ca71ecdf5004b07d4492fcc4eaac621
Tamaroh <tamaroh@gmail.com>
2021-12-22 19:15:34 +0900

    set cors

--- webspeechapitest/zoomapi.js
+++ webspeechapitest/zoomapi.js
@@ -14,7 +14,7 @@ function sendToZoom(msg) {
     const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
     fetch(url, {
         method: 'POST',
-        mode: 'no-cors',
+        mode: 'cors',
         body: msg,
     });
     seq ++ ;
