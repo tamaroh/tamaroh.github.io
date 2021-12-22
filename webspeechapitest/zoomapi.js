75196085bbd452a14f1639643113c89d2a0dde50
Tamaroh <tamaroh@gmail.com>
2021-12-22 17:49:32 +0900

    no-cors post success

--- webspeechapitest/zoomapi.js
+++ webspeechapitest/zoomapi.js
@@ -1,28 +1,50 @@
-const btn = document.getElementById('btn');
+const start = document.getElementById('start');
+const send = document.getElementById('send');
 const content = document.getElementById('content');
 let zoomApiToken;
 let seq = 0;
-btn.addEventListener('click' , useZoom);
-async function useZoom() {
-    // save token
+function setZoom() {
     zoomApiToken = document.getElementById("apitoken").value;
-    // reset seq
-    for (let i = 0; i < 10; i++) {
-        const message = `メッセージテスト: i = ${i}, seq = ${seq}`;
-        const url = zoomApiToken + '&seq=' + i + '&lang=ja-JP';
-        fetch (url, {
-            method: 'POST',
-            mode: 'no-cors',
-            body: message + '\r\n'
-        })
-        .then (res => {
-            content.innerHTML += '<div>'+ message +'</div>\r\n';
-            seq ++;
-            console.log("seq:", seq)
-            await setTimeout(function(){console.log("seq:", seq)}, 3000);
-        });
-    }
 }
+
+function sendToZoom() {
+    const message = "Message test: seq = " + seq;
+    const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: message,
+    }).then(res => {
+        console.log(res);
+    })
+    seq ++ ;
+}
+
+start.addEventListener('click' ,setZoom);
+send.addEventListener('click', sendToZoom)
+// async function useZoom() {
+//     // save token
+//     zoomApiToken = document.getElementById("apitoken").value;
+//     // reset seq
+//     for (let seq = 0; seq < 10; seq++) {
+//         const message = `メッセージテスト: seq = ${seq}`;
+//         const url = zoomApiToken + '&seq=' + seq + '&lang=ja-JP';
+//         fetch (url, {
+//             method: 'POST',
+//             mode : "no-cors",
+//             body: message + '\r\n'
+//         })
+//         .then (res => {
+//             console.log(res);
+//             content.innerHTML += '<div>'+ message +'</div>\r\n';
+//             console.log("seq:", seq)
+//         });
+//         await setTimeout(function(){
+//             console.log("seq:", seq)
+//         }, 3000);
+
+//     }
+// }
 /**
  * Access to fetch at 'https://us02wmcc.zoom.us/closedcaption?id=89390871051&ns=VGFtYXJvaOOBrlpvb23jg5_jg7zjg4bjgqPjg7Pj&expire=86400&sparams=id%2Cns%2Cexpire&signature=-_eY5udIbx4SzvD9pRnoKZhoruaGKDe63KNO5dq6tEM.AG.aDzLw9vAHfsIfYBKnHSAPuUyHJXXvBybiFJxcPAb5PST-8JUDsvdDtQBNmy-HPnFqe84HWjGYPRCMLNLp1ksFfZ0dg2dCjPYmSq4-uIP3RFFmvQZVBtzmwQF629EC80B5ExAPxBYRHY7tvZV3E2KmZFhMtnn5M4r.9sRJBvBjc4iiuN8r7nqtig.JYwFIDJgmKR7zmUH&seq=0&lang=ja-JP' 
  * from origin 'https://tamaroh.github.io' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
